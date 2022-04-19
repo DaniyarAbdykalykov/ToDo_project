2b5582e258cbc843045c8f92c33714267ed4d75b
Daniyar Abdykalykov <dzhalilovich7@gmail.com>
2022-04-19 11:37:46 +0300

    init

---
+++ main/views.py
@@ -0,0 +1,10 @@
+import re
+from django.shortcuts import render, HttpResponse
+
+def homepage(request):
+    return HttpResponse('Hello world')
+
+def test(request):
+    return render(request, 'test.html')
+
+
