567b5be92d13d4584249ed72dee347e62b0fcdd1
Daniyar Abdykalykov <dzhalilovich7@gmail.com>
2022-04-28 17:12:57 +0300

    added ToMeet model to app homework

--- homework/models.py
+++ homework/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# class ToMeet(models.Model):
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    date_of_meeting = models.DateTimeField()
+    comment = models.TextField(null=True, blank=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
 
 
 # Create your models here.
