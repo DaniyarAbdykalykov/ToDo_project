5681eb7342aa3c1c3ca330781cc23a1a539f7920
Daniyar Andykalykov (laptop) <dzhalilovich7@gmail.com>
2022-05-03 17:41:45 +0300

    created meeting.css, link to HTML file and layout ToMeet. Alsolink CSS for homework.html

--- todo/urls.py
+++ todo/urls.py
@@ -31,4 +31,3 @@ urlpatterns = [
     path('meeting/', meeting, name='meeting')
 ]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
